e3eb04b4279bdb22475c63b5072c291ad639be07
ABISHECKGUHAN . G <abisheckguhan94@gmail.com>
2026-06-20 19:05:18 +0530

    Delete new website/frontend directory

--- new website/frontend/script.js
+++
@@ -1,7 +0,0 @@
-function slideStandard(direction) {
-    const wrapper = document.getElementById("standardWrapper");
-    wrapper.scrollBy({
-        left: direction * 420,
-        behavior: "smooth"
-    });
-}
